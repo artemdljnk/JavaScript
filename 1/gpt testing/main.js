b12cbb35a0beba19936768b5f1b82d44067d9071
artemdljnk <artemodessa31.01@gmail.com>
2024-12-11 17:23:06 +0200

    1.1

---
+++ 1/gpt testing/main.js
@@ -0,0 +1,22 @@
+// let name = prompt("What is your name?");
+// let age = Number(prompt("What is your age?"));
+//
+// let person = `Привет, ${name}! Тебе ${age} лет`
+// console.log(person);
+
+// -----------------------------------------------------------------
+
+// let firstNumber = Number(prompt(`Enter first number`));
+// let lastNumber = Number(prompt(`Enter last number`));
+//
+// console.log(firstNumber + lastNumber);
+// console.log(firstNumber - lastNumber);
+// console.log(firstNumber / lastNumber);
+// console.log(firstNumber * lastNumber);
+
+// -------------------------------------------------------------------------
+
+// let str = `123`
+// console.log(+str + 10)
+
+// ---------------------------------------------------------------
