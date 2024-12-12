a0c711a618348511dc8c750e7ba66bfe1f7113ed
artemdljnk <artemodessa31.01@gmail.com>
2024-12-12 19:23:16 +0200

    2.2

---
+++ 2/gpt testing/main.js
@@ -0,0 +1,110 @@
+// let array = [1, 2, 3, 5, 3.14, -999, 'JsTop', 'Artem', false, true];
+//
+// console.log(array[0]);
+// console.log(array[1]);
+// console.log(array[2]);
+// console.log(array[3]);
+// console.log(array[4]);
+// console.log(array[5]);
+// console.log(array[6]);
+// console.log(array[7]);
+// console.log(array[8]);
+// console.log(array[9]);
+
+// --------------------------------------------------------------------------------
+
+// let AllNumbers = [
+//     [1, 2, 3, 4, 5],
+//     [6, 7, 8, 9, 10],
+//     [11, 12, 13, 14, 15],
+// ]
+// console.log(AllNumbers[0])
+
+// ------------------------------------------------------------------
+
+// let FavoriteCar1 = {
+//     brand: 'Audi',
+//     model: 'Rs6',
+//     year: 2023
+// }
+// let FavoriteCar2 = {
+//     brand: 'Bmw',
+//     model: 'M3 GTR',
+//     year: 2000
+// }
+// let FavoriteCar3 = {
+//     brand: 'Tayota',
+//     model: 'Supra',
+//     year: 2015
+// }
+
+// -----------------------------------------------------------------
+
+// let move1 = {
+//     title: 'artem',
+//     duration: 'Artem',
+//     genre: 'Artem',
+//     actors: [
+//         {name: 'Artem', age: 15},
+//     ]
+// }
+//
+// let move2 = {
+//     title: 'nazar',
+//     duration: 'Nazar',
+//     genre: 'Nazar',
+//     actors: [
+//         {name: 'Nazar', age: 11},
+//     ]
+// }
+//
+// let move3 = {
+//     title: 'ivan',
+//     duration: 'Ivan',
+//     genre: 'Ivan',
+//     actors: [
+//         {name: 'Ivan', age: 25},
+//     ]
+// }
+
+// ------------------------------------------------------------------------
+
+// let AllUsers = [
+//     { name: 'Oleg', username: 'oleg_pro', password: 'olegLove' },
+//     { name: 'artem', username: 'artem_pro', password: 'artemLove' },
+//     { name: 'nazar', username: 'nazar_pro', password: 'nazarLove' },
+//     { name: 'boba', username: 'boba_pro', password: 'bobaLove' },
+//     { name: 'biba', username: 'biba_pro', password: 'bibaLove' },
+//     { name: 'danya', username: 'danya_pro', password: 'danyaLove' },
+//     { name: 'dima', username: 'dima_pro', password: 'diamLove' },
+//     { name: 'denis', username: 'denis_pro', password: 'denisLove' },
+//     { name: 'radik', username: 'radik_pro', password: 'radikLove' },
+//     { name: 'valera', username: 'valera_pro', password: 'valeraLove' },
+// ]
+//
+// console.log(AllUsers[0].password)
+// console.log(AllUsers[1].password)
+// console.log(AllUsers[2].password)
+// console.log(AllUsers[3].password)
+// console.log(AllUsers[4].password)
+// console.log(AllUsers[5].password)
+// console.log(AllUsers[6].password)
+// console.log(AllUsers[7].password)
+// console.log(AllUsers[8].password)
+// console.log(AllUsers[9].password)
+
+// --------------------------------------------------------------------------
+
+// let person = {
+//     age: 25 // Задаём возраст
+// };
+//
+// if (person.age < 18) {
+//     console.log("Неполнолетний");
+// } else if (person.age >= 18 && person.age <= 60) {
+//     console.log("Взрослый");
+// } else {
+//     console.log("Пожилой");
+// }
+
+// ------------------------------------------------------------------------------------
---
+++ 2/hw2/main.js
@@ -0,0 +1,107 @@
+// let array = [
+//     1, 2, 10, -999, 3.14, false, true, 'Artem', 'JsTop', 'Hello'
+// ];
+//
+// console.log(array);
+// console.log(array[0]);
+// console.log(array[1]);
+// console.log(array[2]);
+// console.log(array[3]);
+// console.log(array[4]);
+// console.log(array[5]);
+// console.log(array[6]);
+// console.log(array[7]);
+// console.log(array[8]);
+// console.log(array[9]);
+
+// -----------------------------------------------------------
+
+// let book1 = {
+//     title: 'Privet',
+//     pageCount: 12,
+//     genre: 'Fantasy',
+// };
+//
+// let book2 = {
+//     title: 'Hello World',
+//     pageCount: 100,
+//     genre: 'Programming',
+// };
+//
+// let book3 = {
+//     title: 'Adventure Time',
+//     pageCount: 250,
+//     genre: 'Adventure',
+// };
+
+// ------------------------------------------------------------------------
+
+// let book1 = {
+//     title: 'Privet',
+//     pageCount: 12,
+//     genre: 'Fantasy',
+//     authors: [
+//         { name: 'Nazar', age: 33 },
+//         { name: 'Sergey', age: 55 }
+//     ]
+// };
+//
+// let book2 = {
+//     title: 'Hello World',
+//     pageCount: 100,
+//     genre: 'Programming',
+//     authors: [
+//         { name: 'Alice', age: 29 },
+//         { name: 'Bob', age: 45 }
+//     ]
+// };
+//
+// let book3 = {
+//     title: 'Adventure Time',
+//     pageCount: 250,
+//     genre: 'Adventure',
+//     authors: [
+//         { name: 'Jake', age: 40 },
+//         { name: 'Finn', age: 16 }
+//     ]
+// };
+
+// ----------------------------------------------------------------------------------------
+
+// let AllUsers = [
+//     { name: 'Oleg', username: 'oleg_pro', password: 'olegLove' },
+//     { name: 'artem', username: 'artem_pro', password: 'artemLove' },
+//     { name: 'nazar', username: 'nazar_pro', password: 'nazarLove' },
+//     { name: 'boba', username: 'boba_pro', password: 'bobaLove' },
+//     { name: 'biba', username: 'biba_pro', password: 'bibaLove' },
+//     { name: 'danya', username: 'danya_pro', password: 'danyaLove' },
+//     { name: 'dima', username: 'dima_pro', password: 'diamLove' },
+//     { name: 'denis', username: 'denis_pro', password: 'denisLove' },
+//     { name: 'radik', username: 'radik_pro', password: 'radikLove' },
+//     { name: 'valera', username: 'valera_pro', password: 'valeraLove' },
+// ]
+//
+// console.log(AllUsers[0].password)
+// console.log(AllUsers[1].password)
+// console.log(AllUsers[2].password)
+// console.log(AllUsers[3].password)
+// console.log(AllUsers[4].password)
+// console.log(AllUsers[5].password)
+// console.log(AllUsers[6].password)
+// console.log(AllUsers[7].password)
+// console.log(AllUsers[8].password)
+// console.log(AllUsers[9].password)
+
+// ------------------------------------------------------------------------------------------------
+
+// let temperatureWeek = [
+//     { morning: 15, day: 20, evening: 18 }, // Понедельник
+//     { morning: 16, day: 21, evening: 19 }, // Вторник
+//     { morning: 14, day: 22, evening: 20 }, // Среда
+//     { morning: 13, day: 19, evening: 17 }, // Четверг
+//     { morning: 15, day: 23, evening: 21 }, // Пятница
+//     { morning: 12, day: 18, evening: 16 }, // Суббота
+//     { morning: 11, day: 17, evening: 15 }  // Воскресенье
+// ];
+
+// --------------------------------------------------------------------------------------------
