a38a65d24403b80a0a0cec730b1af75ec9440ad3
artemdljnk <artemodessa31.01@gmail.com>
2024-12-21 17:05:49 +0200

    4.4

---
+++ 4/gpt testing/main.js
@@ -0,0 +1,60 @@
+// function sayHello(name) {
+//     return console.log(`Hello, ${name}!`);
+// }
+// sayHello(`Artem`);
+
+// ----------------------------------------------------
+
+// function add(fNum, lNum) {
+//     return fNum + lNum;
+// }
+// console.log(add(5, 3));
+// console.log(add(10, 20));
+
+// --------------------------------------
+
+// function getLength(text) {
+//     return `${text.length}`
+// }
+// console.log(getLength("JavaScript"))
+// console.log(getLength("Привет"))
+
+// --------------------------------------------------------
+
+// function findMin(fNum, lNum) {
+//     if (fNum <= lNum) {
+//         return fNum;
+//     } else {
+//         return lNum;
+//     }
+// }
+// console.log(findMin(15, 11));
+
+// ------------------------------------------
+
+// function countVowels(text) {
+//     const vowels = "aeiouAEIOU"; // Все гласные
+//     let count = 0; // Счетчик гласных
+//     for (let textElement of text) {
+//         if (vowels.includes(textElement)) {
+//             count++; // Увеличиваем счетчик, если символ — гласная
+//         }
+//     }
+//     return count; // Возвращаем количество гласных
+// }
+//
+// console.log(countVowels("JavaScript")); // 3
+// console.log(countVowels("Hello World")); // 3
+// console.log(countVowels("Привет")); // 0
+// untVowels("Привет"));
+
+// --------------------------------------------------
+
+// function reverseString(text) {
+//     return text.split('').reverse().join('');
+// }
+// console.log(reverseString("JavaScript")); // "tpircSavaJ"
+// console.log(reverseString("Hello")); // "olleH"
+// console.log(reverseString("12345")); // "54321"
+
+// ----------------------------------------------------------------------
---
+++ 4/hw4/main.js
@@ -0,0 +1,45 @@
+// function main(a, b) {
+//     return a * b
+// }
+
+// ------------------------------------------------
+
+// function main(r) {
+//     let result = 3.14 * r * r;
+//     return result;
+// }
+
+// -----------------------------------
+
+// function main(h, r) {
+//     return 2 * Math.PI * r * h;
+// }
+
+// -----------------------------------------
+
+// function main(array) {
+//     for (let arrayElement of array) {
+//         console.log(arrayElement)
+//     }
+// }
+
+// -------------------------------------------
+
+// function main(text) {
+//     let result = `<p>${text}</p>`
+//     document.write(result)
+// }
+
+// ------------------------------------------
+
+// function main(text) {
+//     document.write(`
+//     <ul>
+//         <li>${text}</li>
+//         <li>${text}</li>
+//         <li>${text}</li>
+//     </ul>
+//     `);
+// }
+
+// ---------------------------------------------------
